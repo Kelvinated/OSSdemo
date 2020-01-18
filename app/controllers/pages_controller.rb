91ea78ad4396b5f4626b38811b0a36e571af7c86
Kelvin Lee <kelvin_lee@leaderradio.cn>
2020-01-18 15:53:03 +0800

    need new logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,12 @@ class PagesController < ApplicationController
   end
 
   def update
-  # For file attached to has_one_attached
-  # @user.avatar.attach(params[:avatar])
+    # For file attached to has_one_attached
+    # @user.avatar.attach(params[:avatar])
+    @user = User.first
 
-  # For Array of images to has_many_attached
-    params[:friends].each do |friend|
+    # For Array of images to has_many_attached
+    params[:user][:friends].each do |friend|
       @user.friends.attach(friend)
     end
   end
